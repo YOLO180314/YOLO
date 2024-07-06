3cc29349e9479157f196b76a46e3ced4d5ffa413
lapi <1015249773@qq.com>
2024-07-06 14:02:53 +0800

    修改navBar导致的错误 添加返回键 未解决机型适配问题

--- components/navBar1/navBar1.js
+++ components/navBar1/navBar1.js
@@ -1,30 +1,20 @@
-
-// 获取应用实例
-const App = getApp();
 Component({
   properties: {
     title: {
       type: String,
-      value: '情绪创作' //正确的！
+      value: '情绪创作'
     }
   },
-  data:{
-    capsuleTop: '', //胶囊距离屏幕顶部的距离
-    capsuleHeight: '', //胶囊高度
-    navbarHeight: '' //导航栏高度
-    //title:'情绪创作' 错误的！
+
+  data: {
+    statusBarHeight: wx.getStorageSync('statusBarHeight'),
+    navigationBarHeight: wx.getStorageSync('navigationBarHeight'),
+    menuButtonHeight: wx.getStorageSync('menuButtonHeight'),
+    navigationBarAndStatusBarHeight: wx.getStorageSync('statusBarHeight') + wx.getStorageSync('navigationBarHeight'),
   },
-  lifetimes: {
-    attached: function () {
-      this.setData({
-        capsuleTop: App.globalData.capsule.top, 
-        capsuleHeight: App.globalData.capsule.height, 
-        navbarHeight: (App.globalData.capsule.top - App.globalData.system.statusBarHeight) * 2 + App.globalData.capsule.height + App.globalData.system.statusBarHeight, 
-      })
-     }
-  },
+
   methods: {
-    handleGoToBack(){
+    back(){
       wx.navigateBack({
         delta: 1
       })
--- pages/draw/draw.js
+++ pages/draw/draw.js
@@ -1,10 +1,22 @@
+
+ 
 Page({
   data: {
     items: [
-      { src: '../../image/img/情绪创作/book.png', time: '2024-07-01 12:00' },
+     { src: '../../image/img/情绪创作/book.png', time: '2024-07-01 12:00' },
       { src: '../../image/img/情绪创作/book.png', time: '2024-07-01 12:00' }
-     
-    ]
-  }
+    ],
+    statusBarHeight: wx.getStorageSync('statusBarHeight'),
+    navigationBarAndStatusBarHeight: wx.getStorageSync('statusBarHeight') + wx.getStorageSync('navigationBarHeight'),
+  },
 
+  handleBack() {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
+
+  handleSubmit() {
+    // 你的提交处理逻辑
+  }
 });
