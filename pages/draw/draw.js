8960b8f704a63f50edd1f6357fe10f399d9ada2f
CHIYO <3321571675@qq.com>
2024-07-06 16:04:08 +0800

    完善了日记页面

---
+++ pages/draw/draw.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+    items: [
+      { src: '../../image/img/情绪创作/book.png', time: '2024-07-01 12:00' },
+      { src: '../../image/img/情绪创作/book.png', time: '2024-07-01 12:00' }
+     
+    ]
+  }
+
+});
