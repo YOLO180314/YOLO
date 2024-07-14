e42b26f687f175ae6303b8b195730614dc427ef9
lapi <1015249773@qq.com>
2024-07-14 23:54:32 +0800

    正式完成AI绘画功能

--- pages/draw/draw.js
+++ pages/draw/draw.js
@@ -1,22 +1,132 @@
-
 const util = require('../../utils/util.js');
+const db = wx.cloud.database();
+const collection = db.collection('items');
+
+const src = "../../image/img/情绪创作/book.png";
+const url1 = 'https://api-ai.vivo.com.cn/api/v1/task_submit';
+const url2 = 'https://api-ai.vivo.com.cn/api/v1/task_progress';
+const app_id = '3035858207';
+const app_key = 'CVHYGGCJxROxRQAY';
+const dataId = 'wxf0a0b22d358cf4c3'; // 替换为实际的请求唯一ID
+const styleConfig = '55c682d5eeca50d4806fd1cba3628781'; // 替换为实际的风格模板ID
+const URI1 = '/api/v1/task_submit';
+const URI2 = '/api/v1/task_progress';
+
+const CryptoJS = require('crypto-js');
+
+// 生成随机的8位字符串作为nonce
+function generateNonce() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let nonce = '';
+  for (let i = 0; i < 8; i++) {
+    nonce += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return nonce;
+}
+
+// 计算URL中的canonical_query_string 当该项只有 key 时，转换公式为 url_encode(key) + “=” 的形式。
+function generateCanonicalQueryString1(url) {
+  let canonicalQueryString = '';
+  const queryString = url.split('?')[1]; // 获取查询字符串部分
+  
+  if (queryString) {
+    // 解析查询字符串成键值对数组
+    const paramsArray = queryString.split('&').map(pair => pair.split('='));
+    
+    // 对键值对数组按照键名进行排序
+    paramsArray.sort((a, b) => a[0] < b[0] ? -1 : 1);
+    
+    // 构建规范化的查询字符串
+    canonicalQueryString = paramsArray.map(([key, value]) => {
+      return value !== undefined ? `${encodeURIComponent(key)}=${encodeURIComponent(value)}` : `${encodeURIComponent(key)}=`;
+    }).join('&');
+  }
+  
+  return canonicalQueryString;
+}
+
+// 当该项是 key=value 的形式时，转换公式为 url_encode(key) + “=” + url_encode(value) 的形式。
+function generateCanonicalQueryString2(url) {
+  let canonicalQueryString = '';
+  const queryString = url.split('?')[1]; // 获取查询字符串部分
+
+  if (queryString) {
+    // 解析查询字符串成键值对数组
+    const paramsArray = queryString.split('&').map(pair => {
+      const [key, value] = pair.split('=');
+      return [decodeURIComponent(key), decodeURIComponent(value || '')];
+    });
+    
+    // 对键值对数组按照键名进行排序
+    paramsArray.sort((a, b) => a[0] < b[0] ? -1 : 1);
+    
+    // 构建规范化的查询字符串
+    canonicalQueryString = paramsArray.map(([key, value]) => {
+      return value !== undefined ? `${encodeURIComponent(key)}=${encodeURIComponent(value)}` : `${encodeURIComponent(key)}=`;
+    }).join('&');
+  }
+  
+  return canonicalQueryString;
+}
+
+// 计算签名signature 提交作画
+function calculateSignature1(url, app_id, app_key, timestamp, nonce, URI) {
+  // 构建signed_headers_string
+  const signed_headers_string = `x-ai-gateway-app-id:${app_id}\n`
+                                + `x-ai-gateway-timestamp:${timestamp}\n`
+                                + `x-ai-gateway-nonce:${nonce}`;
+
+  // 计算canonical_query_string
+  const canonical_query_string = generateCanonicalQueryString1(url);
+
+  // 构建signing_string
+  const signing_string = `POST\n${URI}\n${canonical_query_string}\n${app_id}\n${timestamp}\n${signed_headers_string}`;
+
+  // 计算 HMAC-SHA256 哈希值
+  const hash = CryptoJS.HmacSHA256(signing_string, app_key);
+
+  // 将结果转换为 Base64 编码
+  const base64Signature = CryptoJS.enc.Base64.stringify(hash);
+
+  return base64Signature;
+}
+
+// 计算签名signature 查询作画
+function calculateSignature2(url, app_id, app_key, timestamp, nonce, URI) {
+  // 构建signed_headers_string
+  const signed_headers_string = `x-ai-gateway-app-id:${app_id}\n`
+                                + `x-ai-gateway-timestamp:${timestamp}\n`
+                                + `x-ai-gateway-nonce:${nonce}`;
+
+  // 计算canonical_query_string
+  const canonical_query_string = generateCanonicalQueryString2(url);
+
+  // 构建signing_string
+  const signing_string = `GET\n${URI}\n${canonical_query_string}\n${app_id}\n${timestamp}\n${signed_headers_string}`;
+
+  // 计算 HMAC-SHA256 哈希值
+  const hash = CryptoJS.HmacSHA256(signing_string, app_key);
+
+  // 将结果转换为 Base64 编码
+  const base64Signature = CryptoJS.enc.Base64.stringify(hash);
+
+  return base64Signature;
+}
 
 Page({
   data: {
-    items: [
-      { src: "../../image/img/情绪创作/book.png", time: "2024年7月3日", src2: '../../image/img/情绪创作/考试结束.jpg'},
-      { src: "../../image/img/情绪创作/book.png", time: "2024年7月5日", src2: 'https://sgw-dx.xf-yun.com/api/v1/sparkdesk/multimodal_image_213088094U047650a7ppv.jpg?authorization=c2ltcGxlLWp3dCBhaz1zcGFya2Rlc2s4MDAwMDAwMDAwMDE7ZXhwPTMyOTc0NjQwNTQ7YWxnbz1obWFjLXNoYTI1NjtzaWc9SGZRdUhvbzJleFVPV29jTUM2dUpMOXRJQ04rSE9zMW1yNUFudXR0ZWF4OD0=&x_location=7YfmxI7B7uKO7jlRxIftd6UZdo=='},
-    ],
+    items: [],
     prompt: '', // 存储输入框的值
     TIME: '', // 新增一个 TIME 属性
-   
   },
   onLoad: function () {
-    var TIME = util.formatTime(new Date());
+    const TIME = util.formatTime(new Date());
     console.log(TIME);
     this.setData({
       TIME: TIME  
     });
+    // 从数据库中读取 items
+    this.loadItemsFromDatabase();
   },
   handleBack: function() {
     wx.navigateBack({
@@ -28,236 +138,167 @@ Page({
       prompt: e.detail.value,
     });
   },
-  
-
-  // 点击发送按钮触发，更新时间并处理提交逻辑
-  submitForm: function () {
-    var TIME = this.data.TIME; // 获取页面中的 TIME 值
-    var prompt=this.data.prompt;
-    submitArtTask(url1, app_id, app_key, dataId, prompt, '', 1, styleConfig, 512, 512, -1, 7, 0.1, 0.5, 20, '', 'JPEG')
-    // 更新 items 数组中的时间信息
-    const newItem = { src: "../../image/img/情绪创作/book.png", time: TIME,src2:'' };
-    this.setData({
-      items: this.data.items.concat(newItem),
-      prompt: '', // 清空输入框内容
+  loadItemsFromDatabase: function() {
+    collection.get({
+      success: (res) => {
+        console.log('数据库读取成功', res.data);
+        if (res.data && res.data.length > 0) {
+          this.setData({
+            items: res.data.map(item => ({
+              src: item.src,
+              time: item.time,
+              src2: item.src2
+            }))
+          });
+        } else {
+          console.log('没有数据');
+        }
+      },
+      fail: (err) => {
+        console.error('数据库读取失败', err);
+      }
     });
-   
   },
 
-});
+  // 点击发送按钮触发，更新时间并处理提交逻辑
+  submitForm: function() {
+    const TIME = this.data.TIME; // 获取页面中的 TIME 值
+    const prompt = this.data.prompt;
 
+    // 提交作画任务
+    this.submitArtTask(url1, app_id, app_key, dataId, prompt, '', 1, styleConfig, 512, 512, -1, 7, 0.1, 0.5, 20, '', 'JPEG'); // 传递 this
+  },
 
-const url1 = 'https://api-ai.vivo.com.cn/api/v1/task_submit';
-const url2 = 'https://api-ai.vivo.com.cn/api/v1/task_progress';
-const app_id = '3035858207';
-const app_key = 'CVHYGGCJxROxRQAY';
-const dataId = 'wxf0a0b22d358cf4c3'; // 替换为实际的请求唯一ID
-var prompt = ''; 
-const styleConfig = '55c682d5eeca50d4806fd1cba3628781'; // 替换为实际的风格模板ID
-const URI1 ='/api/v1/task_submit';
-const URI2='/api/v1/task_progress';
+  // 发送POST请求提交作画任务
+  submitArtTask: function(url, app_id, app_key, dataId, prompt, initImages, imageType, styleConfig, height, width, seed, cfgScale, denoisingStrength, ctrlNetStrength, steps, negativePrompt, imageFormat) {
+    const nonce = generateNonce();
+    const timestamp = Math.floor(Date.now() / 1000); // 当前时间的时间戳，单位秒
+    const signature = calculateSignature1(url, app_id, app_key, timestamp, nonce, URI1);
 
-const CryptoJS = require('crypto-js');
-
-// 生成随机的8位字符串作为nonce
-function generateNonce() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var nonce = '';
-    for (let i = 0; i < 8; i++) {
-        nonce += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return nonce;
-}
-// 计算URL中的canonical_query_string 当该项只有 key 时，转换公式为 url_encode(key) + “=” 的形式。
-function generateCanonicalQueryString1(url) {
-  var canonicalQueryString = '';
-  const queryString = url.split('?')[1]; // 获取查询字符串部分
-  
-  if (queryString) {
-  // 解析查询字符串成键值对数组
-  const paramsArray = queryString.split('&').map(pair => pair.split('='));
-  
-    // 对键值对数组按照键名进行排序
-    paramsArray.sort((a, b) => {
-        return a[0] < b[0] ? -1 : 1;
-    });
-    
-    // 构建规范化的查询字符串
-    canonicalQueryString = paramsArray.map(([key, value]) => {
-        return value !== undefined ? `${encodeURIComponent(key)}=${encodeURIComponent(value)}` : `${encodeURIComponent(key)}=`;
-    }).join('&');
-  }
-  
-  return canonicalQueryString;
-  }
-
-  //当该项是 key=value 的形式时，转换公式为 url_encode(key) + “=” + url_encode(value) 的形式。
-  //将每一项转换后，以 key 按照字典顺序（ ASCII 码由小到大）排序，并使用 & 符号连接起来，生成相应的 canonical_query_string 。
-function generateCanonicalQueryString2(url) {
-  var canonicalQueryString = '';
-  const queryString = url.split('?')[1]; // 获取查询字符串部分
-
-  if (queryString) {
-      // 解析查询字符串成键值对数组
-      const paramsArray = queryString.split('&')
-          .map(pair => {
-              const [key, value] = pair.split('=');
-              return [decodeURIComponent(key), decodeURIComponent(value || '')];
-          });
-      
-      // 对键值对数组按照键名进行排序
-      paramsArray.sort((a, b) => {
-          return a[0] < b[0] ? -1 : 1;
-      });
-      
-      // 构建规范化的查询字符串
-      canonicalQueryString = paramsArray.map(([key, value]) => {
-          return value !== undefined ? `${encodeURIComponent(key)}=${encodeURIComponent(value)}` : `${encodeURIComponent(key)}=`;
-      }).join('&');
-  }
-  
-  return canonicalQueryString;
-}
-
-// 计算签名signature 提交作画
-function calculateSignature1(url, app_id, app_key, timestamp, nonce,URI) {
-  // 构建signed_headers_string
-  const signed_headers_string = `x-ai-gateway-app-id:${app_id}\n`
-                              + `x-ai-gateway-timestamp:${timestamp}\n`
-                              + `x-ai-gateway-nonce:${nonce}`;
-
-  // 计算canonical_query_string
-  const canonical_query_string = generateCanonicalQueryString1(url);
-
-  // 构建signing_string
-  const signing_string = `POST\n${URI}\n${canonical_query_string}\n${app_id}\n${timestamp}\n${signed_headers_string}`;
-
-  // 计算 HMAC-SHA256 哈希值
-const hash = CryptoJS.HmacSHA256(signing_string, app_key);
-
-// 将结果转换为 Base64 编码
-const base64Signature = CryptoJS.enc.Base64.stringify(hash);
-
-  const signature = base64Signature;
-  
-  return signature;
-}
-
-// 计算签名signature 查询作画
-function calculateSignature2(url, app_id, app_key, timestamp, nonce,URI) {
-  // 构建signed_headers_string
-  const signed_headers_string = `x-ai-gateway-app-id:${app_id}\n`
-                              + `x-ai-gateway-timestamp:${timestamp}\n`
-                              + `x-ai-gateway-nonce:${nonce}`;
-
-  // 计算canonical_query_string
-  const canonical_query_string = generateCanonicalQueryString2(url);
-
-  // 构建signing_string
-  const signing_string = `GET\n${URI}\n${canonical_query_string}\n${app_id}\n${timestamp}\n${signed_headers_string}`;
-
-  // 计算 HMAC-SHA256 哈希值
-const hash = CryptoJS.HmacSHA256(signing_string, app_key);
-
-// 将结果转换为 Base64 编码
-const base64Signature = CryptoJS.enc.Base64.stringify(hash);
-
-  const signature = base64Signature;
-  
-  return signature;
-}
-
-// 发送POST请求提交作画任务
-function submitArtTask(url, app_id, app_key, dataId, prompt, initImages, imageType, styleConfig, height, width, seed, cfgScale, denoisingStrength, ctrlNetStrength, steps, negativePrompt, imageFormat) {
-  var nonce=generateNonce();
-  var timestamp = Math.floor(Date.now() / 1000); // 当前时间的时间戳，单位秒
-  var signature = calculateSignature1(url1, app_id, app_key, timestamp, nonce,URI1);
-
-  // 构建请求头
-  var headers = {
-    'Content-Type': 'application/json',
-    'X-AI-GATEWAY-APP-ID': app_id,
-    'X-AI-GATEWAY-TIMESTAMP': timestamp,
-    'X-AI-GATEWAY-NONCE': nonce,
-    'X-AI-GATEWAY-SIGNED-HEADERS': 'x-ai-gateway-app-id;x-ai-gateway-timestamp;x-ai-gateway-nonce',
-    'X-AI-GATEWAY-SIGNATURE': signature,
-  };
+    // 构建请求头
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-AI-GATEWAY-APP-ID': app_id,
+      'X-AI-GATEWAY-TIMESTAMP': timestamp,
+      'X-AI-GATEWAY-NONCE': nonce,
+      'X-AI-GATEWAY-SIGNED-HEADERS': 'x-ai-gateway-app-id;x-ai-gateway-timestamp;x-ai-gateway-nonce',
+      'X-AI-GATEWAY-SIGNATURE': signature,
+    };
 
     // 构建请求体
     const requestData = {
-        dataId: dataId,
-        businessCode: 'pc',
-        prompt: prompt || '',
-        initImages: initImages || '',
-        imageType: imageType || 1,
-        styleConfig: styleConfig,
-        height: height || 512,
-        width: width || 512,
-        seed: seed || -1,
-        cfgScale: cfgScale || 7,
-        denoisingStrength: denoisingStrength || 0.1,
-        ctrlNetStrength: ctrlNetStrength || 0.5,
-        steps: steps || 20,
-        negativePrompt: negativePrompt || '',
-        imageFormat: imageFormat || 'JPEG',
+      dataId: dataId,
+      businessCode: 'pc',
+      prompt: prompt || '',
+      initImages: initImages || '',
+      imageType: imageType || 1,
+      styleConfig: styleConfig,
+      height: height || 512,
+      width: width || 512,
+      seed: seed || -1,
+      cfgScale: cfgScale || 7,
+      denoisingStrength: denoisingStrength || 0.1,
+      ctrlNetStrength: ctrlNetStrength || 0.5,
+      steps: steps || 20,
+      negativePrompt: negativePrompt || '',
+      imageFormat: imageFormat || 'JPEG',
     };
+
     // 发送请求
     wx.request({
-        url: url,
-        method: 'POST',
-        header: headers,
-        data: requestData,
-        success: function (res) {
-            console.log('提交作画任务成功', res.data);
-            queryTaskStatus(res.data.result.task_id);
-        },
-        fail: function (error) {
-            console.error('提交作画任务失败', error);
-            // 处理请求失败
-        }
-    });
-}
-
-/// 查询作画任务状态的函数
-function queryTaskStatus(taskId) {
-  // 构建完整的请求 URL
-var fullUrl = url2 + '?task_id=' + taskId;
-
-// 添加必要的签名信息和头部信息
-var nonce = generateNonce();
-var timestamp = Math.floor(Date.now() / 1000);
-var signature = calculateSignature2(fullUrl, app_id, app_key, timestamp, nonce, URI2);
-
-wx.request({
-  url: fullUrl,
-  method: 'GET',
-  header: {
-    'X-AI-GATEWAY-APP-ID': app_id,
-    'X-AI-GATEWAY-TIMESTAMP': timestamp,
-    'X-AI-GATEWAY-NONCE': nonce,
-    'X-AI-GATEWAY-SIGNED-HEADERS': 'x-ai-gateway-app-id;x-ai-gateway-timestamp;x-ai-gateway-nonce',
-    'X-AI-GATEWAY-SIGNATURE': signature,
-  },
-    success: function(res) {
-      console.log('查询作画任务成功', res.data);
-      var result = res.data.result;
-      if (result.status !== 0) {
-        wx.showToast({
-          title: '正在作画中...', 
-          icon: 'loading', 
-          mask: true 
-        });
-      } else {
-        wx.hideToast();
+      url: url,
+      method: 'POST',
+      header: headers,
+      data: requestData,
+      success: (res) => {
+        console.log('提交作画任务成功', res.data);
+        this.queryTaskStatus(res.data.result.task_id);  // 使用箭头函数，确保 this 上下文正确
+      },
+      fail: (error) => {
+        console.error('提交作画任务失败', error);
+        // 处理请求失败
       }
-    },
-    fail: function(error) {
-      console.error('查询作画任务失败', error);
-      wx.hideToast(); 
+    });
+  },
+
+  queryTaskStatus: function(taskId) {
+    const that = this; // 保存 Page 实例的引用
+    const fullUrl = url2 + '?task_id=' + taskId;
+
+    // 添加必要的签名信息和头部信息
+    const nonce = generateNonce();
+    const timestamp = Math.floor(Date.now() / 1000);
+    const signature = calculateSignature2(fullUrl, app_id, app_key, timestamp, nonce, URI2);
+
+    // 定义轮询的时间间隔
+    const interval = 2000; // 2秒
+    const maxRetries = 30; // 最大尝试次数
+    let retries = 0;
+
+    function checkStatus() {
+      wx.request({
+        url: fullUrl,
+        method: 'GET',
+        header: {
+          'X-AI-GATEWAY-APP-ID': app_id,
+          'X-AI-GATEWAY-TIMESTAMP': timestamp,
+          'X-AI-GATEWAY-NONCE': nonce,
+          'X-AI-GATEWAY-SIGNED-HEADERS': 'x-ai-gateway-app-id;x-ai-gateway-timestamp;x-ai-gateway-nonce',
+          'X-AI-GATEWAY-SIGNATURE': signature,
+        },
+        success: (res) => {
+          const result = res.data.result;
+          if (!result.finished) { // 修改判断条件
+            // 如果任务未完成，则继续轮询
+            if (retries < maxRetries) {
+              retries++;
+              wx.showToast({
+                title: '请耐心等待....',
+                icon: 'loading',
+                mask: true
+              });
+              setTimeout(checkStatus, interval); // 重新调用checkStatus进行轮询
+            } else {
+              wx.hideToast();
+              wx.showToast({
+                title: '任务超时，请重试。',
+                icon: 'none',
+              });
+            }
+          } else {
+            console.log('查询作画任务成功', res.data);
+            wx.hideToast();
+            // 获取作画结果的图片URL
+            const imageUrl = result.images_url[0]; // 只取第一个图片的URL
+            const TIME = util.formatTime(new Date()); // 格式化时间
+
+            collection.add({
+              data: {
+                prompt: that.data.prompt,
+                time: TIME,
+                src: src,
+                src2: imageUrl
+              },
+              success: (res) => {  // 使用箭头函数，确保 this 上下文正确
+                console.log('数据保存成功', res);
+                that.setData({
+                  prompt: '', // 清空输入框内容
+                });
+                that.loadItemsFromDatabase();
+              },
+              fail: (err) => {
+                console.error('数据保存失败', err);
+              }
+            });
+          }
+        },
+        fail: (error) => {  // 使用箭头函数，确保 this 上下文正确
+          console.error('查询作画任务失败', error);
+          wx.hideToast();
+        }
+      });
     }
-  });
 
-    }
-
-
-    
+    checkStatus(); // 启动轮询
+  }
+});
